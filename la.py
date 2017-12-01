5c17e39bb10ec76769215b6eda2410ecebf7753a
Eric Bower <neurosnap@gmail.com>
2017-11-30 23:13:32 -0500

    chapter 3 work

--- la.py
+++ la.py
@@ -1,5 +1,5 @@
 import math
-from functools import reduce
+from functools import reduce, partial
 
 def vector_2d(point_one, point_two):
     (x1, y1) = point_one
@@ -38,6 +38,28 @@ def euclidean_length(vector):
     dot = dot_product(vector, vector)
     return math.sqrt(dot)
 
+def set_matrix(matrix, value=0):
+    columns = len(matrix)
+    rows = len(matrix[0])
+    return [[value] * rows for _ in range(columns)]
+
+zero_matrix = partial(set_matrix, value=0)
+one_matrix = partial(set_matrix, value=1)
+
+def set_to_diagonal(matrix, value=None):
+    new_matrix = []
+    for i, col in enumerate(matrix):
+        new_col = []
+        for j, row in enumerate(col):
+            val = 0
+            if i == j:
+                val = value if value else row
+            new_col.append(val)
+        new_matrix.append(new_col)
+    return new_matrix
+
+set_to_identity = partial(set_to_diagonal, value=1)
+
 if __name__ == '__main__':
     assert add_vectors((4, -3), (1, 5)) == [5, 2]
     assert add_vectors([-1, 2], [-3, -2]) == [-4, 0]
@@ -53,4 +75,21 @@ if __name__ == '__main__':
     assert dot_product((1, 1, 1, 1), vec_1) == 12
 
     assert euclidean_length((1, -2, 2)) == 3.0
+
+    actual_matrix = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    expected_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    assert zero_matrix(actual_matrix) == expected_matrix
+
+    actual_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    expected_matrix = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    assert one_matrix(actual_matrix) == expected_matrix
+
+    actual_matrix = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    expected_matrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    assert set_to_identity(actual_matrix) == expected_matrix
+
+    actual_matrix = [[2, 3, 4], [5, 6, 7], [8, 9, 10]]
+    expected_matrix = [[2, 0, 0], [0, 6, 0], [0, 0, 10]]
+    assert set_to_diagonal(actual_matrix) == expected_matrix
+
     print('All tests pass :)')
